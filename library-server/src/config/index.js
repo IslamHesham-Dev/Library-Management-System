f710cd1768a4577c534d50075d86b3b4d897b170
Islam Hesham Kamel Ali Ismail <islam.ismail@student.giu-uni.de>
2025-04-05 10:57:59 +0200

    Update index.js

--- library-server/src/config/index.js
+++ library-server/src/config/index.js
@@ -2,8 +2,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
-const MONGO_URL = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@localhost:27017/librarydb`;
-//const MONGO_URL: string = "mongodb://user:password@localhost:27017/librarydb";
+const MONGO_URL = process.env.MONGO_URL || '';
+//for local const MONGO_URL = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@localhost:27017/librarydb`;
 const PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 8000;
 const ROUNDS = process.env.SERVER_ROUNDS ? Number(process.env.SERVER_ROUNDS) : Math.floor(Math.random() * 11);
 export const config = {
